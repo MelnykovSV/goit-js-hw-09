2d15428ea9c4c1dcd3c382a9d912b1c55d435ced
MelnykovSV <114297826+MelnykovSV@users.noreply.github.com>
2023-02-04 18:45:40 +0000

    Deploying to gh-pages from @ MelnykovSV/goit-js-hw-09@36597b7e3ded39016b3c3ae55872a5dacae4e3f5 🚀

---
+++ 01-color-switcher.41d6e8d3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),o=document.querySelector("[data-stop]");function e(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(()=>{const o=setInterval(e,1e3);t.disabled=!0,console.log(o)})),o.addEventListener("click",(()=>{console.log(id)}));
+//# sourceMappingURL=01-color-switcher.41d6e8d3.js.map
