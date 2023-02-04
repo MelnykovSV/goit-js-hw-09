fb519455c666b1bedeb0e43efa1ad0fc50ceca5b
MelnykovSV <114297826+MelnykovSV@users.noreply.github.com>
2023-02-04 20:27:19 +0000

    Deploying to gh-pages from @ MelnykovSV/goit-js-hw-09@8251c5d25e8514a9b41daa396700850b57561e30 🚀

--- 01-color-switcher.de60ae7e.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),o=document.querySelector("[data-stop]");function e(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){var o=setInterval(e,1e3);t.disabled=!0,console.log(o)})),o.addEventListener("click",(function(){console.log(id)}))}();
-//# sourceMappingURL=01-color-switcher.de60ae7e.js.map
