6d7e26ce133ad2f903fc0ba5431657e161c73297
MelnykovSV <svmelnykov@gmail.com>
2023-02-05 20:09:28 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,11 +3,11 @@ const form = document.querySelector('.form');
 form.addEventListener('submit', e => {
   e.preventDefault();
   const data = new FormData(e.currentTarget);
-  FormData.keys(data);
-  // data.forEach(ele => {
-  //   console.log(ele.name);
-  //   console.log(ele.value);
-  // });
+
+  for (item of [...data]) {
+    console.log(`${item[0]} value: ${item[1]}`);
+  }
+  form.reset();
 });
 
 function createPromise(position, delay) {
