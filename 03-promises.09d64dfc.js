0e432583b18e52393144f0f41130c90b2abab4bb
MelnykovSV <114297826+MelnykovSV@users.noreply.github.com>
2023-02-05 18:06:36 +0000

    Deploying to gh-pages from @ MelnykovSV/goit-js-hw-09@b56d9f264bf4ffe29087568e51846a6d1fb49806 🚀

---
+++ 03-promises.09d64dfc.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault();var t=new FormData(e.currentTarget);FormData.keys(t)}));
+//# sourceMappingURL=03-promises.09d64dfc.js.map
---
+++ 03-promises.3914c51a.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(e=>{e.preventDefault();const t=new FormData(e.currentTarget);FormData.keys(t)}));
+//# sourceMappingURL=03-promises.3914c51a.js.map
