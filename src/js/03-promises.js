c180397f2e28d155f0663de2d1c7048a2b48c78b
MelnykovSV <svmelnykov@gmail.com>
2023-02-05 20:54:07 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,12 +3,28 @@ const form = document.querySelector('.form');
 form.addEventListener('submit', e => {
   e.preventDefault();
   const data = new FormData(e.currentTarget);
+  const params = {};
 
   for (item of [...data]) {
     console.log(`${item[0]} value: ${item[1]}`);
+    params[item[0]] = item[1];
   }
+
   form.reset();
+
+  buildDelaysArray(params);
 });
+
+//creates array of steps from data
+
+function buildDelaysArray({ delay, step, amount }) {
+  const delays = [];
+  for (let i = 0; i < amount; i++) {
+    delays.push(delay + i * step);
+  }
+  console.log(delays);
+  console.log(delays.length);
+}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
