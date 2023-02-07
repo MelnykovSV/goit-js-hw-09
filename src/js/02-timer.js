205d11a461a0bb535c2ef0a6d813a8837cdd52a7
MelnykovSV <svmelnykov@gmail.com>
2023-02-07 23:11:57 +0200

    compressing

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import Notiflix from 'notiflix';
+import { convertMs } from './convertMs';
 // Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
 
@@ -40,50 +41,26 @@ const options = {
 
 flatpickr(datepicker, options);
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
 //Function to count time left to some date and to write it to fields
 function countTime(time) {
   const timeToCount = time - Date.now();
-  if (Math.floor(timeToCount / 1000)) {
-    console.log(timeToCount);
-    fields.forEach(writeTime);
-  } else {
-    // console.log(`This is the time: ${timeToCount}`);
-    fields.forEach(field => {
-      field.querySelector('.value').textContent = convertMs(timeToCount)
-        [field.querySelector('.label').textContent.toLowerCase()].toString()
-        .padStart(2, '0');
-    });
-    // console.log(`Final time ${Math.floor(timeToCount / 1000)}`);
-    // console.log(
-    //   `Should be in value: ${convertMs(timeToCount)
-    //     [fields[3].querySelector('.label').textContent.toLowerCase()].toString()
-    //     .padStart(2, '0')}`
-    // );
+  fields.forEach(writeTime);
+  if (!Math.floor(timeToCount / 1000)) {
     clearInterval(timerID);
-    setTimeout(Notiflix.Notify.warning('The time has come'), 1000);
+    Notiflix.Notify.warning('The time has come');
   }
 
+  // if (Math.floor(timeToCount / 1000)) {
+  //   fields.forEach(writeTime);
+  // } else {
+  //   fields.forEach(writeTime);
+  //   clearInterval(timerID);
+  //   Notiflix.Notify.warning('The time has come');
+  // }
+
+  //Function write time to fields
+
   function writeTime(formField) {
-    console.log(formField);
     formField.querySelector('.value').textContent = convertMs(timeToCount)
       [formField.querySelector('.label').textContent.toLowerCase()].toString()
       .padStart(2, '0');
