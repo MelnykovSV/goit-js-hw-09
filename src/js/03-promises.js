734266034fc63e7a1d649a81e610da5569e1bac7
MelnykovSV <svmelnykov@gmail.com>
2023-02-08 02:22:28 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,36 +3,14 @@ import Notiflix from 'notiflix';
 const form = document.querySelector('.form');
 
 form.addEventListener('submit', e => {
-  e.preventDefault();
-  const data = new FormData(e.currentTarget);
-  const params = {};
+  const formObject = geatherFormData(e);
 
-  for (item of [...data]) {
-    console.log(`${item[0]} value: ${item[1]}`);
-    params[item[0]] = item[1];
-  }
+  const delaysArray = buildDelaysArray(formObject);
 
-  form.reset();
+  const promisesArray = delaysArray.map(generatePromise);
 
-  const delays = buildDelaysArray(params);
-
-  const promisesArray = delays.map((delay, position) => {
-    const promise = new Promise((resolve, reject) => {
-      const shouldResolve = Math.random() > 0.3;
-      setTimeout(() => {
-        if (shouldResolve) {
-          resolve(`✅ Fulfilled promise ${position + 1} in ${delay}ms`);
-        } else {
-          reject(`❌ Rejected promise ${position + 1} in ${delay}ms`);
-        }
-      }, delay);
-    });
-
-    return promise;
-  });
-
-  for (ele of promisesArray) {
-    ele
+  for (promise of promisesArray) {
+    promise
       .then(a => {
         Notiflix.Notify.success(a);
       })
@@ -41,6 +19,20 @@ form.addEventListener('submit', e => {
       });
   }
 });
+
+function geatherFormData(e) {
+  e.preventDefault();
+  const data = new FormData(e.currentTarget);
+  const formObject = {};
+
+  for (item of [...data]) {
+    console.log(`${item[0]} value: ${item[1]}`);
+    formObject[item[0]] = item[1];
+  }
+
+  form.reset();
+  return formObject;
+}
 
 //creates array of steps from data
 
@@ -52,4 +44,22 @@ function buildDelaysArray({ delay, step, amount }) {
 
   return delays;
 }
-Notiflix.Notify.info('Cogito ergo sum');
+
+//generates single promise with given delay
+
+function generatePromise(delay, position) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position + 1} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position + 1} in ${delay}ms`);
+      }
+    }, delay);
+  });
+
+  return promise;
+}
+
+// Notiflix.Notify.info('Cogito ergo sum');
